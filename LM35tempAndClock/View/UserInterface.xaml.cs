1663f1c9f8d1db74cd05dddaec5500a02ca04d14
C0505812 <C0505812@TB205A-11>
2022-12-05 20:47:38 -0800

    multiple page GUI and virtual LEDs

---
+++ LM35tempAndClock/View/UserInterface.xaml.cs
@@ -0,0 +1,193 @@
+using System.IO.Ports;
+using System.Net.Sockets;
+using System.Text;
+using Windows.Devices.Enumeration;
+
+namespace LM35tempAndClock.View;
+
+public partial class UserInterface : ContentPage
+{
+    private bool bPortOpen = false;
+    private string newPacket = "";
+    private int oldPacketNumber = -1;
+    private int newPacketNumber = 0;
+    private int lostPacketCount = 0;
+    private int packetRollover = 0;
+    private int chkSumError = 0;
+
+    SerialPort serialPort = new SerialPort();
+    StringBuilder stringBuilderSend = new StringBuilder("###1111196");
+
+    LMclass lmClass = new LMclass();
+    public UserInterface()
+    {
+        InitializeComponent();
+
+        string[] ports = SerialPort.GetPortNames();
+        portPicker.ItemsSource = ports;
+        portPicker.SelectedIndex = ports.Length;
+        Loaded += MainPage_Loaded;
+    }
+
+    private void MainPage_Loaded(object sender, EventArgs e)
+    {
+        serialPort.BaudRate = 115200;
+        serialPort.ReceivedBytesThreshold = 1;
+        serialPort.DataReceived += SerialPort_DataReceived;
+
+    }
+
+    private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        newPacket = serialPort.ReadLine();
+        MainThread.BeginInvokeOnMainThread(MyMainThreadCode);
+    }
+
+    private void MyMainThreadCode()
+    {
+
+        int calChkSum = 0;
+        if (newPacket.Length > 37)
+        {
+
+            if (newPacket.Substring(0, 3) == "###")
+            {
+                newPacketNumber = Convert.ToInt32(newPacket.Substring(3, 3));
+
+                if (oldPacketNumber > -1)
+                {
+                    if (newPacketNumber < oldPacketNumber)
+                    {
+                        packetRollover++;
+                        if (oldPacketNumber != 999)
+                        {
+                            lostPacketCount += 999 - oldPacketNumber + newPacketNumber;
+                        }
+                    }
+                    else
+                    {
+                        if (newPacketNumber != oldPacketNumber + 1)
+                        {
+                            lostPacketCount += newPacketNumber - oldPacketNumber;
+                        }
+                    }
+                }
+                for (int i = 3; i < 34; i++)
+                {
+                    calChkSum += (byte)newPacket[i];
+                }
+                calChkSum %= 1000;
+                int recChkSum = Convert.ToInt32(newPacket.Substring(34, 3));
+                if (recChkSum == calChkSum)
+                {
+                    Temperature(newPacket);
+                    HighSensor(lmClass.avgAnalogValue(newPacket, 0));
+                    oldPacketNumber = newPacketNumber;
+                }
+                else
+                {
+                    chkSumError++;
+                }
+
+                string parsedData = $"{newPacket.Length,-14}" +
+                                   $"{newPacket.Substring(0, 3),-14}" +
+                                   $"{newPacket.Substring(3, 3),-14}" +
+                                   $"{newPacket.Substring(6, 4),-14}" +
+                                   $"{newPacket.Substring(10, 4),-14}" +
+                                   $"{newPacket.Substring(14, 4),-14}" +
+                                   $"{newPacket.Substring(18, 4),-14}" +
+                                   $"{newPacket.Substring(22, 4),-14}" +
+                                   $"{newPacket.Substring(26, 4),-14}" +
+                                   $"{newPacket.Substring(30, 4),-14}" +
+                                   $"{newPacket.Substring(34, 3),-17}" +
+                                   $"{calChkSum,-19}" +
+                                   $"{lostPacketCount,-11}" +
+                                   $"{chkSumError,-14}" +
+                                   $"{packetRollover,-14}\r\n";
+
+            }
+
+        }
+    }
+
+    private void Temperature(string validPacket)
+    {
+        double temperature = lmClass.GetTemperature(lmClass.avgAnalogValue(validPacket, 0));
+        labelTemperature.Text = temperature.ToString("  00.0") + " °C";
+    }
+
+    public void HighSensor(double voltage)
+    {
+        double temperature = (voltage / 10);
+        if (temperature > 25)
+        {
+            stringBuilderSend[3] = '0';
+            labelWarning.Text = "  To Hot!";
+            imgLED1.Source = "ledon.png";
+        }
+        else if (temperature < 24.7)
+        {
+            stringBuilderSend[3] = '1';
+            labelWarning.Text = "  Okay";
+            imgLED1.Source = "ledoff.png";
+        }
+        sendPacket();
+    }
+    private void btnOpenClose_Clicked(object sender, EventArgs e)
+    {
+        if (!bPortOpen)
+        {
+            serialPort.PortName = portPicker.SelectedItem.ToString();
+            serialPort.Open();
+            btnOpenClose.Text = "Close";
+            bPortOpen = true;
+        }
+        else
+        {
+            serialPort.Close();
+            btnOpenClose.Text = "Open";
+            bPortOpen = false;
+        }
+    }
+
+    private async void btnSend_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            string messageOut = entrySend.Text;
+            messageOut += "\r\n";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageOut);
+            serialPort.Write(messageBytes, 0, messageBytes.Length);
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Alert", ex.Message, "OK");
+        }
+
+    }
+    private void sendPacket()
+    {
+        int calSendChkSum = 0;
+        try
+        {
+            for (int i = 3; i < 7; i++)
+            {
+                calSendChkSum += (byte)stringBuilderSend[i];
+            }
+            calSendChkSum %= 1000;
+            stringBuilderSend.Remove(7, 3);
+            stringBuilderSend.Insert(7, calSendChkSum.ToString());
+            string messageOut = stringBuilderSend.ToString();
+            entrySend.Text = stringBuilderSend.ToString();
+            messageOut += "\r\n";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageOut);
+            serialPort.Write(messageBytes, 0, messageBytes.Length);
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Alert", ex.Message, "OK");
+        }
+
+    }
+}
+
