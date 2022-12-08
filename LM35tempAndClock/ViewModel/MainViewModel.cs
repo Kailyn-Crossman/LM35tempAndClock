05a177d1053e368d9be408714fd17cb5fe8f32d6
C0505812 <C0505812@TB205A-11>
2022-12-07 17:28:08 -0800

    trying to sort out mvvm

---
+++ LM35tempAndClock/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+
+
+namespace LM35tempAndClock.ViewModel
+{
+    [ObservableObject]
+    public partial class MainViewModel
+    {
+       /* string[] ports = SerialPort.GetPortNames();
+        portPicker.ItemsSource = ports;
+        portPicker.SelectedIndex = ports.Length;*/
+    }
+}
